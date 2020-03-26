72db84ab33b6414fa5a67d759b735ac091ce9485
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-26 18:16:30 -0400

    update teacher class

--- src/school/management/system/Teacher.java
+++ src/school/management/system/Teacher.java
@@ -6,10 +6,37 @@ public class Teacher {
     private String name;
     private int salary;
 
-    public Teacher(int id, String name, int salary){
+
+    /**
+     * creates a new teacher object
+     *
+     * @param id     teacher id : unique
+     * @param name   teacher name
+     * @param salary salary of the teacher
+     */
+
+    public Teacher(int id, String name, int salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
+
+    /**
+     *
+     * @return id for the current teacher
+     */
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
 
 }
