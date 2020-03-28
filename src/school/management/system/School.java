a07e31ae94969b4575576c760a4872e5dbd7cb50
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-28 16:39:27 -0400

    array list

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,4 +1,10 @@
 package school.management.system;
-
+// school can have many teachers, many students
+// implement teachers and students by using an array list
 public class School {
+
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
 }
