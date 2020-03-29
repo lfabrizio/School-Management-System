480b0913febd24a1ac297f42982e2b33d6855703
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-29 17:23:39 -0400

    finished

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -11,12 +11,12 @@ public class Main {
 
     public static void main(String[] args) {
 	Teacher bray = new Teacher(1, "bray", 500);
-	Teacher drew = new Teacher(2, "drew", 700);
+	Teacher randy = new Teacher(2, "randy", 700);
 	Teacher shayna = new Teacher(3, "shayna", 600);
 
 	List<Teacher> teacherList = new ArrayList<>();
 	teacherList.add(bray);
-	teacherList.add(drew);
+	teacherList.add(randy);
 	teacherList.add(shayna);
 
 	Student roman = new Student(1, "roman", 4);
@@ -36,6 +36,10 @@ public class Main {
     System.out.println("shs has earned $" + shs.getTotalMoneyEarned());
 
 		System.out.println(" ----making shs salary----");
+		bray.receiveSalary(bray.getSalary());
+		System.out.println("shs has spent for salary to " + bray.getName()+ "and now has $" + shs.getTotalMoneyEarned());
 
+		randy.receiveSalary(randy.getSalary());
+		System.out.println("shs has spent for salary to "+ randy.getName()+" and now has $" + shs.getTotalMoneyEarned());
     }
 }
