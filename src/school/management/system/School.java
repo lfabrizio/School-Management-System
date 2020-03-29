c8a33527a4dd298fbf5bf7efc6cb9efa9af6e0e8
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-29 14:15:10 -0400

    getter

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -18,5 +18,46 @@ public class School {
     public School(List<Teacher> teachers, List<Student> students) {
         this.teachers = teachers;
         this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    /**
+     * @return the list of teachers in the school
+     */
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+
+    /** add a teacher to the school
+     * @param teacher the teacher to be added
+     */
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    /**
+     * @return the list of students in the school
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /** adds a student to the school
+     * @param student to be added
+     */
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
     }
 }
