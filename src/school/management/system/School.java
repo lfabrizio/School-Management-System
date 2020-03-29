fc737e8a2e58c73515e314bcb5428a536fcb4e17
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-03-28 21:10:09 -0400

    school class

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,10 +1,22 @@
 package school.management.system;
+
+import java.util.List;
+
 // school can have many teachers, many students
 // implement teachers and students by using an array list
 public class School {
 
-    private Teacher teacher;
-    private Student student;
+    private List<Teacher> teachers;
+    private List<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpent;
+
+    /** new school object is created
+     * @param teachers list of teachers
+     * @param students list of students in the school
+     */
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+    }
 }
